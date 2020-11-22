4417f4f0ff0a075607c5b06d3f427453fc091aac
Kyle Martin <kymartin@csumb.edu>
2020-11-21 23:30:56 -0800

    Insert customer data into DB

--- charge.php
+++ charge.php
@@ -1,7 +1,8 @@
 <?php
     require_once('vendor/autoload.php');
     require_once('config/db.php');
-    require_once('lib/pd_db.php');
+    require_once('lib/pdo_db.php');
+    require_once('models/Customer.php');
 
     \Stripe\Stripe::setApiKey('sk_test_51Hpq4CIkuecL7QkUnVXbFdWYIskjwYpA8248lJ0sffdPBqslK7uzOScCG6RCXNhW3ZkziF7CJYEx65dVzAO9TZgf001jfrLSwH');
 
@@ -27,6 +28,20 @@
         "description" => "Test Product",
         "customer" => $customer->id
     ));
+
+// Customer data
+$customerData = [
+    'id' => $charge->customer, 
+    'first_name' => $first_name,
+    'last_name' => $last_name,
+    'email' => $email
+];
+
+// Instatiate customer object
+$customer = new Customer();
+
+// Add customer to DB
+$customer->addCustomer($customerData);
 
 //Redirect to success page 
 header('Location: success.php?tid='.$charge->id.'&product='.$charge->description);
