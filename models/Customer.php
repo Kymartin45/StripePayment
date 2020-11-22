d64efdcca9d48efcd535957cdcb448b93531aef0
Kyle Martin <kymartin@csumb.edu>
2020-11-21 22:52:45 -0800

    Bind customer values and execute func implemented

--- models/Customer.php
+++ models/Customer.php
@@ -4,11 +4,24 @@
 
         public function __construct() {
             // From pdo_db.php
-            $this->db = new Database
+            $this->db = new Database;
         }
 
         public function addCustomer($data) {
             // Prepare query to add customer data to db
             $this->db->query('INSERT INTO customers (id, first_name, last_name, email) VALUES():id, :first_name, :last_name, :email)');
+
+            //Bind values from pdo_db.php  
+            $this->db->bind(':id', $data['id']);
+            $this->db->bind(':first_name', $data['first_name']);
+            $this->db->bind(':last_name', $data['last_name']);
+            $this->db->bind(':email', $data['email']);
+
+            // Execute function
+            if($this->db->execute()) {
+                return true;
+            } else {
+                return false;
+            }
         } 
     }
